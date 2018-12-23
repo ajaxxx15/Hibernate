922f374430eae590e99d49c783c68f8898e43a4b
Somee <Somee@somee-pc.mshome.net>
2018-12-23 14:41:34 +0530

    First Commit

---
+++ hibernate3/src/p1/Book.java
@@ -0,0 +1,32 @@
+package p1;
+
+public class Book {
+	private int bid;
+	private String bname;
+	private Publisher publisher;
+
+	public int getBid() {
+		return bid;
+	}
+
+	public void setBid(int bid) {
+		this.bid = bid;
+	}
+
+	public String getBname() {
+		return bname;
+	}
+
+	public void setBname(String bname) {
+		this.bname = bname;
+	}
+
+	public Publisher getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(Publisher publisher) {
+		this.publisher = publisher;
+	}
+
+}
---
+++ hibernate3/src/p1/BookMain.java
@@ -0,0 +1,34 @@
+package p1;
+
+/**
+ * Many-to-one Mapping
+ *
+ */
+public class BookMain {
+
+	public static void main(String[] args) {
+
+		Book b1 = new Book();
+		Book b2 = new Book();
+		Book b3 = new Book();
+
+		b1.setBname("Java");
+		b2.setBname("C++");
+		b3.setBname("C");
+
+		Publisher p = new Publisher();
+		p.setPname("Pearson");
+
+		b1.setPublisher(p);
+		b2.setPublisher(p);
+		b3.setPublisher(p);
+
+		HibernateManager hm = new HibernateManager();
+		hm.connect();
+		hm.insert(b1);
+		hm.insert(b2);
+		hm.insert(b3);
+
+	}
+
+}
