5baadaaa3f0fd5c15f255cff96c6626aa805a682
Somee <Somee@somee-pc.mshome.net>
2018-12-23 14:42:22 +0530

    First Commit

---
+++ hibernate4/src/p1/HibernateManager.java
@@ -0,0 +1,32 @@
+package p1;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+import java.util.*;
+
+public class HibernateManager {
+	Session session;
+
+	public void connect() {
+		// Connection code
+		// these 5 lines would remain same and is used for getting connection
+		Configuration configuration = new Configuration().configure("hibernate.cfg.xml"); // connects to cfg
+		ServiceRegistryBuilder builder = new ServiceRegistryBuilder().applySettings(configuration.getProperties());
+		SessionFactory sessionFactory = configuration.buildSessionFactory(builder.buildServiceRegistry());
+
+		session = sessionFactory.openSession();
+		System.out.println("Connected to Pf....");
+	}
+
+	// inserts a row into the table
+	public void insert(Student s) {
+		session.beginTransaction();
+		session.save(s);
+		session.getTransaction().commit();
+		System.out.println("Insertion success...");
+	}
+
+}
